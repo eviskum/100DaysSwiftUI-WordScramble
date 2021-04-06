406b91c8f4dfb305f3bf4d25ed4f869b50e2b6b9
Esben Viskum <viskum@viskum.dk>
2021-04-06 19:43:22 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Esben Viskum on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
